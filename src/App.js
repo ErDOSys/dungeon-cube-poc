2fc9aa097859e417dcb4fd33b01c73d149482b6b
ErDOSys <91054440+ErDOSys@users.noreply.github.com>
2022-10-16 21:04:29 +0000

    added helper

---
+++ src/App.js
@@ -0,0 +1,5 @@
+
+// Import dependencies
+import React, { useRef, useState, useEffect } from "react";
+import * as tf from "@tensorflow/tfjs";
+import * as cocossd from "@tensorflow-models/coco-ssd";
