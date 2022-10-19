e2874161a83e85430bff6bf27e3d2776b8b6d39a
ErDOSys <91054440+ErDOSys@users.noreply.github.com>
2022-10-19 18:37:21 +0000

    * working snapshot

---
+++ src/utilities.js
@@ -0,0 +1,14 @@
+
+export const drawRect = (detections, ctx) =>{
+    // Loop through each prediction
+    detections.forEach(prediction => {
+  
+      // Extract boxes and classes
+      const [x, y, width, height] = prediction['bbox']; 
+      const text = prediction['class']; 
+  
+      // Set styling
+      const color = Math.floor(Math.random()*16777215).toString(16);
+      ctx.strokeStyle = '#' + color
+      ctx.font = '18px Arial';
+  
